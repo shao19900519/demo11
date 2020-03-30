3bb5ed8a8bb2309ccfeab1626e5b7de91cfee514
vigoss Shao <927672668@qq.com>
2020-03-30 13:07:08 +0800

    Revert "Merge branch 'master' of github.com:shao19900519/demo11"
    This reverts commit 169246948067c9bdb5b15049ccfed69698b50809, reversing
    changes made to bd21c8531a87e42a3555b6799fd16a2ab74fb2e1.
    
    # Conflicts:
    #	SEC.php

--- SEC.php
+++ SEC.php
@@ -8,8 +8,12 @@
 	<h3>测试能不能更新</h3>
 	<h4>测试获取与拉取的区别</h4>
 	<h4>测试获取与拉取的区别</h4>
+<<<<<<< HEAD
 	<h4>提交不推送，github没有改变</h4>
 	<h4>直接推送会报错</h4>
 	<h4>提交后推送</h4>
+=======
+>>>>>>> parent of 1692469... Merge branch 'master' of github.com:shao19900519/demo11
+
 </body>
 </html>
